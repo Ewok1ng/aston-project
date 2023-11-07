af64495b7a9fa9bdbe34f572cc1c2aab744fc7ce
Ewok1ng <joker187566@gmail.com>
2023-11-07 13:51:06 +0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Main, History } from './pages';
+import { Link } from 'react-router-dom';
 
 function App() {
-    return <div className="app">app</div>;
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <header>
+                    <Link to={'/'}>Main</Link>
+                    <Link to={'/history'}>History</Link>
+                </header>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/history" element={<History />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
